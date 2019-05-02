f8ab10bc9dea2795f3a4c2ad652f0e7abc51b65b
tanakaworld <yutaro.tanaka.world@gmail.com>
2019-05-02 18:38:06 +0900

    chore: fix DOM error by jsdom
    [vue-test-utils]: window is undefined, vue-test-utils needs to be run in a browser environment.
        You can run the tests in node using jsdom
        See https://vue-test-utils.vuejs.org/guides/#browser-environment for more details.

--- test/setup/jest-setup.js
+++ test/setup/jest-setup.js
@@ -3,3 +3,6 @@ const { toMatchImageSnapshot } = require('jest-image-snapshot')
 // Extend Jest's expect
 // https://github.com/americanexpress/jest-image-snapshot#usage
 expect.extend({ toMatchImageSnapshot })
+
+// https://vue-test-utils.vuejs.org/guides/#choosing-a-test-runner
+require('jsdom-global')()
